e749a649417077df19c7596dafbbe30757a5e373
MohamedMohamed <m01126165016@icloud.com>
2024-07-22 01:07:33 +0300

    Update

---
+++ src/Product.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Product() {
+    return (
+        <div>
+            <span>Name</span>
+            <span className='badge badge-warning m-2'>5</span>
+            <span>Product</span>
+            <span>Product</span>
+        </div>
+    )
+}
